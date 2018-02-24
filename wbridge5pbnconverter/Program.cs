3ea6f8ecfb1f4e078f69e01227a5500f98faceaa
Guy <Guy@localhost>
2018-02-24 08:55:43 +0200

    added support for the 5 different hand orders that can occur...

--- wbridge5pbnconverter/Program.cs
+++ wbridge5pbnconverter/Program.cs
@@ -6,33 +6,33 @@ namespace wbridge5pbnconverter
 {
     class Program
     {
-        enum Series { spades, hearts, diamonds, Clubs };
-        public static char[] refe = new char[] { '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K' };
+        enum Series { Spades, Hearts, Diamonds, Clubs };
+        public static char[] reference = new char[] { '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K' };
 
         static void Main(string[] args)
-
         {
             string filename;
             if (args.Length > 0)
                 filename = args[0];
             else
                 filename = @"C:\Users\3stra\Dropbox\Guy\BridgeHands\hand.pbn";
+            Console.WriteLine("Input the card order: type \r\n S for SHCD\r\n H for HSDC\r\n " +
+                "D for DSHC\r\n C for CHSD\r\n  SHDC or 'Enter' for standard input order (no reordring will be perfomed)");
+            string order = Console.ReadLine().ToUpper();
             var lines = from line in File.ReadLines(filename)
                             //where line.Contains("[Deal ")
                         select new
                         {
                             Line = line
                         };
-            using (StreamWriter sw = File.CreateText(filename.Substring(0,filename.Length-4)+"PROCESSED.pbn"))
+            using (StreamWriter sw = File.CreateText(filename.Substring(0, filename.Length - 4) + "PROCESSED.pbn"))
             {
                 foreach (var l in lines)
-
                 {
-
                     if (l.Line.Contains("[Deal "))
                     {
 
-                        string s = FixDeal(l.Line);
+                        string s = FixDeal(l.Line,order);
                         Console.WriteLine("Pre Procesed Deal: " + l.Line);
                         Console.WriteLine("     Post Process: " + s);
                         sw.WriteLine(s);
@@ -44,13 +44,21 @@ namespace wbridge5pbnconverter
                     }
                 }
             }
-            //string code = myString.Substring(myString.IndexOf(toBeSearched) + toBeSearched.Length);
             Console.ReadLine();
         }
 
 
-        public static string FixDeal(string deal, bool fix = true)
+        public static string FixDeal(string deal, string order)
         {
+            if (order == "")
+                order = "SHDC";//standard pbn with no reordering.
+
+            string spades, hearts, clubs, diamonds;
+            HashSet<char> spadeHash = new HashSet<char>();
+            HashSet<char> heartHash = new HashSet<char>();
+            HashSet<char> diamondHash = new HashSet<char>();
+            HashSet<char> clubHash = new HashSet<char>();
+
             string s = deal.Substring(9).ToUpper();
             s = s.Substring(0, s.Length - 2);
             var splithands = s.Split(' ');
@@ -63,29 +71,81 @@ namespace wbridge5pbnconverter
 
             foreach (var hand in splithands)
             {
+                //count cards - report shortness or excess
                 if (hand.Length > 16)
                     Alert(ConsoleColor.Yellow, "hand :" + hand + " is too long");
                 else if (hand.Length < 16)
                     Alert(ConsoleColor.Yellow, "hand :" + hand + " is too short");
                 var cardsets = hand.Split('.');
-                if (fix)
+                
+                //reorder hands
+                switch (order)
                 {
-                    string clubs = cardsets[2];
-                    cardsets[2] = cardsets[3];
-                    cardsets[3] = clubs;
+                    case "S":
+                        spades = cardsets[0];
+                        hearts = cardsets[1];
+                        clubs = cardsets[2];
+                        diamonds = cardsets[3];
+                        break;
+                    case "H":
+                        hearts = cardsets[0];
+                        spades = cardsets[1];
+                        diamonds = cardsets[2];
+                        clubs = cardsets[3];
+                        break;
+                    case "D":
+                        diamonds = cardsets[0];
+                        spades = cardsets[1];
+                        hearts = cardsets[2];
+                        clubs = cardsets[3];
+                        break;
+                    case "C":
+                        clubs = cardsets[0];
+                        hearts = cardsets[1];
+                        spades = cardsets[2];
+                        diamonds = cardsets[3];
+                        break;
+                    case "SHDC":
+                        spades = cardsets[0];
+                        hearts = cardsets[1];
+                        diamonds = cardsets[2];
+                        clubs  = cardsets[3];
+                        break;
+                    default:
+                        Console.WriteLine("You requested a weird deck order");
+                        return deal;
                 }
-                for (int i = 0; i < 4; i++)
-                    foreach (char c in cardsets[i])
-                        if (!h[i].Add(c)) //report duplicate cards.
-                            Alert(ConsoleColor.Red, ">>>> ++ " + c.ToString() + "  is duplicate in " + (Series)(i + 1 - Convert.ToInt32(fix)));
 
+                //append hand cards to newdeal in correct pbn order
+                newdeal += spades + "." + hearts + "." + diamonds + "." + clubs + " ";
 
-                newdeal += cardsets[0] + "." + cardsets[1] + "." + cardsets[2] + "." + cardsets[3] + " ";
+                //push into hashes and report duplicates
+                foreach (char c in spades)
+                    if (!spadeHash.Add(c))
+                        Alert(ConsoleColor.Red, ">>>> ++ " + c.ToString() + "  is duplicate in Spades");
+                foreach (char c in hearts)
+                    if (!heartHash.Add(c))
+                        Alert(ConsoleColor.Red, ">>>> ++ " + c.ToString() + "  is duplicate in Hearts");
+                foreach (char c in diamonds)
+                    if (!diamondHash.Add(c)) 
+                        Alert(ConsoleColor.Red, ">>>> ++ " + c.ToString() + "  is duplicate in Diamonds");
+                foreach (char c in clubs)
+                    if (!clubHash.Add(c)) 
+                        Alert(ConsoleColor.Red, ">>>> ++ " + c.ToString() + "  is duplicate in Clubs");
             }
-            for (int i = 0; i < 4; i++)
-                foreach (var c in refe)
-                    if (!h[i].Contains(c)) //report missing cards.
-                        Alert(ConsoleColor.Green, ">>>> -- " + c + " is missing in " + (Series)(i + 1 - Convert.ToInt32(fix)));
+            //cross hashes against reference to identify the specific missing card(s).
+            foreach (var c in reference)
+                if (!spadeHash.Contains(c)) //report missing cards.
+                    Alert(ConsoleColor.Green, ">>>> -- " + c + " is missing in Spades");
+            foreach (var c in reference)
+                if (!heartHash.Contains(c)) //report missing cards.
+                    Alert(ConsoleColor.Green, ">>>> -- " + c + " is missing in Hearts");
+            foreach (var c in reference)
+                if (!diamondHash.Contains(c)) //report missing cards.
+                    Alert(ConsoleColor.Green, ">>>> -- " + c + " is missing in Diamonds");
+            foreach (var c in reference)
+                if (!clubHash.Contains(c)) //report missing cards.
+                    Alert(ConsoleColor.Green, ">>>> -- " + c + " is missing in Clubs");
 
             newdeal = "[Deal \"N:" + newdeal + "\"]";
             //[Deal "N:KJ3.JT74.QJ32.76 62.A95.K96.JT832 Q74.KQ2.AT85.KQ4 AT985.863.74.A95"]
